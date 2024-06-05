f8df56722a5d57a4bbfc02d16889cbd1fabf02bd
Alestln <itworkplace46@gmail.com>
2024-06-05 15:28:29 +0300

    [FEATURE] Added router-outlet !!!!!!!!! Added tailwind

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UserProfileComponent} from "./components/user-profile/user-profile.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'user-profile',
+    children: [
+      {
+        path: 'details/:id',
+        component: UserProfileComponent
+      }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
